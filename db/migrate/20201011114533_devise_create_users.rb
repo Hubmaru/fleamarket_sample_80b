dea153e2c614234f86983a46728bed108cbdce98
chika-tsubaru <k67e5b9fd7zh1@softbank.ne.jp>
2020-10-11 21:02:30 +0900

    add-colum-to-user-table & create-validates in user.rb & rails db:migrate

--- db/migrate/20201011114533_devise_create_users.rb
+++ db/migrate/20201011114533_devise_create_users.rb
@@ -4,8 +4,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :nickname,               null: false
+      t.string :email,                  null: false, default: ""
+      t.string :encrypted_password,     null: false, default: ""
+      t.string :real_family_name,       null: false
+      t.string :real_last_name,         null: false
+      t.string :real_family_name_kana,  null: false
+      t.string :real_last_name_kana,    null: false
+      t.date :birth,                    null: false
 
       ## Recoverable
       t.string   :reset_password_token
@@ -36,6 +42,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.timestamps null: false
     end
 
+    add_index :users, :nickname,             unique: true
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
