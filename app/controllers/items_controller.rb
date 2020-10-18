78684d4ccf26d090b21d11aad8846c8cfacb7c91
mityuuroko <genius320320@gmail.com>
2020-10-18 17:05:28 +0900

    Listingitems

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,25 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  def purchases
+  end
 end
