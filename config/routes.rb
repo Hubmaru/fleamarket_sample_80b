fcfb27c525c54b9202a4281dea9c056a9487b50d
iwa700 <rizasaikou6@yahoo.co.jp>
2020-10-21 15:52:20 +0900

    10th commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  root 'items#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+  resources :items do
+    collection do
+      get 'purchases'
+    end
+  end
+end 
