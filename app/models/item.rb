5d8e35c2046135747f7c4029d23bb26597a02d66
chika-tsubaru <k67e5b9fd7zh1@softbank.ne.jp>
2020-11-08 17:55:52 +0900

    create mypage layout

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
-  has_many: images, dependent: :destroy
-  belongs_to: seller, class_name: :user
-  belongs_to: buyer, class_name: :user
+  has_many :images, dependent: :destroy
+  belongs_to :seller, class_name: :user
+  belongs_to :buyer, class_name: :user
 end
