1af52253edcc18c64451409e916b6bf938d218ea
chika-tsubaru <k67e5b9fd7zh1@softbank.ne.jp>
2020-11-11 08:34:03 +0900

    create loguout redirect view

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    registrations: 'users/registrations'
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
   }
   devise_scope :user do
     get 'delivery_address', to: 'users/registrations#new_delivery_address'
     post 'delivery_address', to: 'users/registrations#create_delivery_address'
+    delete 'destroy_user_session', to: 'users/sessions#destroy'
   end
   root 'items#index'
   resources :items do
