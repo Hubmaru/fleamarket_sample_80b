ffeea1068ed56b8f31046e9ea7a45e238a479a7a
chika-tsubaru <k67e5b9fd7zh1@softbank.ne.jp>
2020-11-11 10:07:16 +0900

    create card new link & view

--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,11 @@ Rails.application.routes.draw do
     end
   end
   resources :users, only: :show
+  resources :card, only: [:new, :show] do
+    collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
+      post 'delete', to: 'card#delete'
+    end
+  end
 end
