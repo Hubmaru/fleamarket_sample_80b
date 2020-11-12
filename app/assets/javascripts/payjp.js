dc7c9644ef04c517bc9fd4b16e7e97d12a2ee456
chika-tsubaru <k67e5b9fd7zh1@softbank.ne.jp>
2020-11-12 10:03:53 +0900

    now JS not active

--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -1,4 +1,5 @@
 document.addEventListener(
+  console.log('OK')
   "DOMContentLoaded", e => {
     if (document.getElementById("token_submit") != null) { //token_submitというidがnullの場合、下記コードを実行しない
       Payjp.setPublicKey("pk_test_79ae2d2743199a76f3eb36aa"); //ここに公開鍵を直書き
