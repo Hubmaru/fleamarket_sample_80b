71358f32e465c0237bfcaed76d40d3a40b24edf1
chika-tsubaru <k67e5b9fd7zh1@softbank.ne.jp>
2020-12-02 19:03:45 +0900

    delete unnecessarey routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-
-  get 'buyers/index'
-  get 'buyers/done'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
