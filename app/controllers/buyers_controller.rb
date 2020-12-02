ee591bd489e83d6cc55c4d9d5ddda53ca85fdccc
chika-tsubaru <k67e5b9fd7zh1@softbank.ne.jp>
2020-12-02 14:04:15 +0900

    create purchase scss

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -11,52 +11,27 @@ class BuyersController < ApplicationController
       flash[:alert] = '購入にはクレジットカード登録が必要です'
     else
       Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-      #保管した顧客IDでpayjpから情報取得
       customer = Payjp::Customer.retrieve(@card.customer_id) 
-      #カード情報表示のためインスタンス変数に代入
       @default_card_information = customer.cards.retrieve(@card.card_id)
+      @exp_month = @default_card_information.exp_month.to_s
+      @exp_year = @default_card_information.exp_year.to_s.slice(2,3)
+      @user = User.find(params[:id])
+      @prefecture = @user.delivery_address.prefecture
+      @municipality = @user.delivery_address.municipality
+      @address = @user.delivery_address.address
+      @delivery_family_name = @user.delivery_address.delivery_family_name
+      @delivery_last_name = @user.delivery_address.delivery_last_name
     end
   end
-
-  # def pay
-  #   @item = Item.find(params[:item_id])
-  #   # 以下の処理は、ビジネスロジックとして、モデルに書き写すことを検討する。
-  #   # すでに購入されていないか？
-  #   if @item.buyer.present? 
-  #     redirect_back(fallback_location: root_path) 
-  #   elsif @card.blank?
-  #     # カード情報がなければ、買えないから戻す
-  #     redirect_to action: "new"
-  #     # フラッシュメッセージを導入する
-  #     flash[:alert] = '購入にはクレジットカード登録が必要です'
-  #   else
-  #     # 購入者なし、クレジットカードもあるし、決済処理に移行
-  #     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-  #     # 請求を発行
-  #     Payjp::Charge.create(
-  #     amount: @item.price,
-  #     customer: @card.customer_id,
-  #     currency: 'jpy',
-  #     )
-  #     # 売り切れなので、productの情報をアップデートして売り切れにします。
-  #     if @item.update(buyer_id: current_user.id)
-  #       flash[:notice] = '購入しました。'
-  #       redirect_to controller: 'items', action: 'show', id: @item.id
-  #     else
-  #       flash[:alert] = '購入に失敗しました。'
-  #       redirect_to controller: 'items', action: 'show', id: @it.id
-  #     end
-  #   end
-  # end
 
   def pay
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
-      :amount => @item.price, #支払金額を引っ張ってくる
-      :customer => @card.customer_id,  #顧客ID
-      :currency => 'jpy',              #日本円
+      :amount => @item.price,
+      :customer => @card.customer_id,
+      :currency => 'jpy',
     )
-    redirect_to done_item_buyer_path #完了画面に移動
+    redirect_to done_item_buyer_path
   end
 
   def done
--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -18,21 +18,14 @@ class CardController < ApplicationController
 
   def create
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-    # jsで作成したpayjpTokenがちゃんと入っているか？
     if params['payjpToken'].blank?
-      # トークンが空なら戻す
-      # binding.pry
       render "new"
     else
-      # トークンがちゃんとあれば進めて、PAY.JPに登録されるユーザーを作成します。
       customer = Payjp::Customer.create(
         description: 'test',
-        # email: current_user.email,
         card: params['payjpToken'],
         metadata: {user_id: current_user.id}
       )
-
-      # PAY.JPのユーザーが作成できたので、creditcardモデルを登録します。
       @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if @card.save
         redirect_to action: "index", notice:"支払い情報の登録が完了しました"
@@ -43,13 +36,10 @@ class CardController < ApplicationController
   end
 
   def destroy     
-    # 今回はクレジットカードを削除するだけでなく、PAY.JPの顧客情報も削除する。
-    # PAY.JPの秘密鍵をセットして、PAY.JPから情報をする。
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-    # PAY.JPの顧客情報を取得
     customer = Payjp::Customer.retrieve(@card.customer_id)
     customer.delete
-    @card.destroy # App上でもクレジットカードを削除
+    @card.destroy
     redirect_to action: "index", notice: "削除しました"
   end
 
