9952e954edb4e3dffbeaa7430eda8ad174bbfaf1
EikoNakashima <68436861+EikoNakashima@users.noreply.github.com>
2020-12-09 16:35:27 +0900

    edit-JS-FIX

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,8 @@ class ItemsController < ApplicationController
   before_action :get_category, only: [:new, :create, :edit, :update]
   before_action :get_size, only: [:new, :create, :edit, :update]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :ensure_current_user, only:[:edit, :update]
+
 
   def index
     @items = Item.includes(:images).order('created_at DESC')
@@ -40,7 +42,7 @@ class ItemsController < ApplicationController
   end
 
   def update
-    if @item.update(edit_params)
+    if @item.update(item_params)
       redirect_to root_path, notice: '商品を編集しました'
     else
       flash.now[:alert] = '必須事項を入力してください'
@@ -83,13 +85,16 @@ class ItemsController < ApplicationController
     @size_parent = Size.where(ancestry: nil)
   end
 
+  def ensure_current_user
+    item = Item.find(params[:id])
+    if item.seller_id != current_user.id
+      redirect_to action: :index
+    end
+  end
+
 
   def item_params
     params.require(:item).permit(:name, :text, :price, :brand, :prefecture_id, :category_id, :size_id, :condition_id, :cost_id, :days_id, images_attributes: [:photo, :_destory, :id]).merge(seller_id: current_user.id)
-  end
-
-  def edit_params
-    params.require(:item).permit(:name, :text, :price, :brand, :prefecture_id, :size_id, :condition_id, :cost_id, :days_id, images_attributes: [:photo, :_destory, :id]).merge(seller_id: current_user.id)
   end
 
 end
